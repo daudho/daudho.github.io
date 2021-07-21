a40e97fbc17c8c19aa29f0c8330707596d967c8f
Daud Ho <daudsyahiran@gmail.com>
2021-07-22 03:27:10 +0800

    Updates

---
+++ precache-manifest.153c58f478fc99140496beaa5ea21dff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63913f01814b7c5c49946ccd248f22d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cbc1434d2b0b9b1f515",
+    "url": "/static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "cca6a260f665e1fd9c85",
+    "url": "/static/css/main.f4eee7c5.chunk.css"
+  },
+  {
+    "revision": "9cbc1434d2b0b9b1f515",
+    "url": "/static/js/2.67586b0e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.67586b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca6a260f665e1fd9c85",
+    "url": "/static/js/main.0b242f2b.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
